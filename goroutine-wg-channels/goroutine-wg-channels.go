99e0aacb78ecd5fddb1d32a0c9b8362b76c72206
pranavmanole26 <pranavmanole26@gmail.com>
2024-08-08 23:05:41 +0530

    added comparison for use of waitgroups and channels in goroutines for sync

---
+++ goroutine-wg-channels/goroutine-wg-channels.go
@@ -0,0 +1,40 @@
+package goroutinewgchannels
+
+import (
+	"sync"
+)
+
+func syncWithWaitGroups() {
+	var wg sync.WaitGroup
+	var varA string
+	var varB string
+
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		varA = "a"
+	}()
+	go func() {
+		defer wg.Done()
+		varB = "b"
+	}()
+
+	wg.Wait()
+
+	_, _ = varA, varB
+}
+
+func syncWithChannels() {
+	var varACh chan string = make(chan string)
+	var varBCh chan string = make(chan string)
+
+	go func() {
+		varACh <- "a"
+	}()
+	go func() {
+		varBCh <- "b"
+	}()
+
+	_, _ = <-varACh, <-varBCh
+}
---
+++ goroutine-wg-channels/goroutine-wg-channels_test.go
@@ -0,0 +1,15 @@
+package goroutinewgchannels
+
+import "testing"
+
+func BenchmarkSyncWithWaitGroups(t *testing.B) {
+	for i := 0; i < t.N; i++ {
+		syncWithWaitGroups()
+	}
+}
+
+func BenchmarkSyncWithChannels(t *testing.B) {
+	for i := 0; i < t.N; i++ {
+		syncWithChannels()
+	}
+}
